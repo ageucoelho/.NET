4baf4a289261a5d3e59d2cdd2b9c37cf4c432baa
ageucoelho <ageulima2010@gmail.com>
2025-03-31 22:49:47 -0300

    add archives

---
+++ Variaveis/Variaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Olá, eu sou uma frase.";
+            string letra = "a";
+            string numero = "100";
+
+            int num1 = 10;
+            int num2 = -20;
+            double num4 = 4.5;
+
+            bool verdadeiro = true;
+            bool falso = false;
+
+            Console.WriteLine(string.Format(frase) + " Estou aprendendo a programar em c#.");
+        }
+    }
+}
---
+++ condicoes/condicoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condicoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome? ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + string.Format(nome) + ". Qual a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 2)
+            {
+                Console.WriteLine("Você é um bebê.");
+            }
+            else if (idade < 10)
+            {
+                Console.WriteLine("Você é uma criança.");
+            }
+            else if (idade < 18)
+            {
+                Console.WriteLine("Você é um adolescente.");
+            }
+            else if (idade < 60)
+            {
+                Console.WriteLine("Voce é um adulto.");
+            }
+            else
+            {
+                Console.WriteLine("Você é um idoso");
+            }
+        }
+    }
+}
